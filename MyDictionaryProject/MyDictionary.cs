64a340a18a961e1aec0f9103a4f4c11467338b1c
Tufan Ozan Karahan <tufanozan1@outlook.com>
2023-09-20 18:28:00 +0300

    Add project files.

---
+++ MyDictionaryProject/MyDictionary.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionaryProject
+{
+    internal class MyDictionary<TKey, TValue>
+    {
+        TKey[] keys;
+        TValue[] values;
+        public MyDictionary()
+        {
+            keys = new TKey[0];
+            values = new TValue[0];
+        }
+
+        public void Add(TKey item, TValue item2)
+        {
+            TKey[] TempKeys = keys;
+            TValue[] TempValues = values;
+
+            keys = new TKey[keys.Length + 1];
+
+            for (int i = 0; i < TempKeys.Length; i++)
+            {
+                keys[i] = TempKeys[i];
+            }
+
+            keys[keys.Length - 1] = item;
+
+            values = new TValue[keys.Length + 1];
+
+            for (int i = 0; i < TempValues.Length; i++)
+            {
+                values[i] = TempValues[i];
+            }
+
+            values[keys.Length - 1] = item2;
+
+            Console.WriteLine("{0}: {1}", item, item2);
+        }
+    }
+}
---
+++ MyDictionaryProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using MyDictionaryProject;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+        myDictionary.Add(12, "Ali");
+        myDictionary.Add(24, "Emre");
+        myDictionary.Add(26, "Atilla");
+    }
+}
